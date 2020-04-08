cfaaccc66f7967dd0b478a00823e85d12c421d1e
pietia <piotrstepak@gmail.com>
2020-04-08 17:44:43 +0200

    Add wall detection

--- Game.java
+++ Game.java
@@ -11,32 +11,41 @@ class Game extends KeyAdapter {
         Board board = new Board(player.getX(), player.getY(), player);
         Obstacle.createObstacle();// tutaj tak dla spradzenia na szybko przeszkoda
         clearScreen();
-        System.out.println(board);
+        // System.out.println(board);
 
         switch(ch) {
             case 'w':
                 // player.moveUp(board);
-                player.move(-1, 0);
+                if (!(board.getCoordinates(player.getX() - 1, player.getY()) instanceof Collidable)) {
+                    player.move(-1, 0);
+                }
                 break;
 
             case 's':
                 // player.moveDown(board);
-                player.move(1, 0);
+                if (!(board.getCoordinates(player.getX() + 1, player.getY()) instanceof Collidable)) {
+                    player.move(1, 0);
+                }
                 break;
 
             case 'a':
                 // player.moveLeft(board);
-                player.move(0, -1);
+                if (!(board.getCoordinates(player.getX(), player.getY() - 1) instanceof Collidable)) {
+                    player.move(0, -1);
+                } 
                 break;
 
             case 'd':
                 // player.moveRight(board);
-                player.move(0, 1);
+                if (!(board.getCoordinates(player.getX(), player.getY() + 1) instanceof Collidable)) {
+                    player.move(0, 1);
+                }
                 break;
 
         }
         // board.setSquare(player.getX(), player.getY(), Icon.PLAYER);
-        // System.out.println(board);
+        board.setPlayerOnBoard(player.getX(), player.getY(), player); 
+        System.out.println(board);
         // System.out.println(player);
     }
 
