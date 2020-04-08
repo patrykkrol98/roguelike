e94de0c3ee03941a7bf7655150b718a3765cbd2e
Dominik-Wojcik <domek99@gmail.com>
2020-04-08 12:31:58 +0200

    add auto walking

--- Enemy.java
+++ Enemy.java
@@ -1,15 +1,58 @@
-
-public class Enemy extends Coordinates{
+public class Enemy extends Coordinates {
     private String name;
     private int health;
     private int attack;
+
     public Enemy(String name, int health, int attack, int x, int y) {
         super(x, y);
         this.name = name;
         this.health = health;
         this.attack = attack;
     }
+
     public void walkHorizontal(Square[][] board) {
-        if (!(board[getX() + 1][getY()] instanceof Collidable)) { setX(getX() + 1); }
+        boolean horizontalDirection = true;
+        horizontalDirection = horizontalDirection ? walkRight(board) : walkLeft(board);
+    }
+
+    private boolean walkRight(Square[][] board) {
+        if (!(board[getX() + 1][getY()] instanceof Collidable)) {
+            setX(getX() + 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean walkLeft(Square[][] board) {
+        if (!(board[getX() - 1][getY()] instanceof Collidable)) {
+            setX(getX() - 1);
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void walkVertically(Square[][] board) {
+        boolean vertivalDirection = true;
+        vertivalDirection = vertivalDirection ? walkUp(board) : walkDown(board);
+    }
+
+    private boolean walkUp(Square[][] board) {
+        if (!(board[getX()][getY() - 1] instanceof Collidable)) {
+            setY(getY() - 1);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean walkDown(Square[][] board) {
+        if (!(board[getX()][getY() + 1] instanceof Collidable)) {
+            setY(getY() + 1);
+            return false;
+        } else {
+            return true;
+        }
     }
 }
